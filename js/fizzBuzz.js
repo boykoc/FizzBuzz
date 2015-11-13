be7d4428312a0bd72909d4274884c5cc0a40242f
Cody Boyko <codyboyko@yahoo.ca>
2015-11-13 12:49:19 -0500

    Replace ng-change with ng-submit as ng-change happens immediately. Fix if statements in change().

--- js/fizzBuzz.js
+++ js/fizzBuzz.js
@@ -1,8 +1,17 @@
 angular.module('fizzBuzzApp', [])
 	.controller('FizzBuzzController', ['$scope', function($scope) {
-		$scope.results = [];
 		$scope.change = function() {
-			$scope.results.push($scope.yourNumber);
-			console.log($scope.results);
+			$scope.results = []; // reset results array each change
+			for (var i = 1; i <= $scope.yourNumber; i++) {
+				if (i%3 == 0 && i%5 == 0) {
+					$scope.results.push("FizzBuzz");
+				} else if (i%3 == 0) {
+					$scope.results.push("Fizz");
+				} else if (i%5 == 0) {
+					$scope.results.push("Buzz");
+				} else {
+					$scope.results.push(i);
+				}				
+			};
 		};
 	}]);
